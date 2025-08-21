33d52a8742c07cbb14bc3579da7cccee4e838636
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-21 14:57:38 -0500

    update: implement `list_tools` and `call_tool` in `mcp_client.py`

--- mcp_client.py
+++ mcp_client.py
@@ -8,10 +8,10 @@ from mcp.client.stdio import stdio_client
 
 class MCPClient:
     def __init__(
-        self,
-        command: str,
-        args: list[str],
-        env: Optional[dict] = None,
+            self,
+            command: str,
+            args: list[str],
+            env: Optional[dict] = None,
     ):
         self._command = command
         self._args = args
@@ -42,14 +42,13 @@ class MCPClient:
         return self._session
 
     async def list_tools(self) -> list[types.Tool]:
-        # TODO: Return a list of tools defined by the MCP server
-        return []
+        result = await self.session().list_tools()
+        return result.tools
 
     async def call_tool(
-        self, tool_name: str, tool_input: dict
+            self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
@@ -78,11 +77,12 @@ class MCPClient:
 # For testing
 async def main():
     async with MCPClient(
-        # If using Python without UV, update command to 'python' and remove "run" from args.
-        command="uv",
-        args=["run", "mcp_server.py"],
+            # If using Python without UV, update command to 'python' and remove "run" from args.
+            command="uv",
+            args=["run", "mcp_server.py"],
     ) as _client:
-        pass
+        result = await _client.list_tools()
+        print(result)
 
 
 if __name__ == "__main__":
